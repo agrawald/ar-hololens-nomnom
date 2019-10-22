67d9fa00bd03d3f0812cccc169aab287ef836917
dagrawal <agrawal.dheeraj.7@gmail.com>
2019-10-22 21:36:33 +1100

    First commit

---
+++ Assets/Scripts/VisionManager.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class VisionManager : MonoBehaviour
+{
+    public static VisionManager instance;
+
+    // you must insert your service key here!    
+    private string authorizationKey = "b07e92fc351940e8b4a95d77818dbdee";
+    private const string ocpApimSubscriptionKeyHeader = "Ocp-Apim-Subscription-Key";
+    private string visionAnalysisEndpoint = "https://australiaeast.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Tags";   // This is where you need to update your endpoint, if you set your location to something other than west-us.
+
+    internal byte[] imageBytes;
+
+    internal string imagePath;
+
+    [System.Serializable]
+    public class TagData
+    {
+        public string name;
+        public float confidence;
+    }
+
+    [System.Serializable]
+    public class AnalysedObject
+    {
+        public TagData[] tags;
+        public string requestId;
+        public object metadata;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        // allows this instance to behave like a singleton
+        instance = this;
+    }
+
+    /// <summary>
+    /// Returns the contents of the specified file as a byte array.
+    /// </summary>
+    private static byte[] GetImageAsByteArray(string imageFilePath)
+    {
+        FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+        BinaryReader binaryReader = new BinaryReader(fileStream);
+        return binaryReader.ReadBytes((int)fileStream.Length);
+    }
+
+    /// <summary>
+    /// Call the Computer Vision Service to submit the image.
+    /// </summary>
+    public IEnumerator AnalyseLastImageCaptured()
+    {
+        WWWForm webForm = new WWWForm();
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Post(visionAnalysisEndpoint, webForm))
+        {
+            // gets a byte array out of the saved image
+            imageBytes = GetImageAsByteArray(imagePath);
+            unityWebRequest.SetRequestHeader("Content-Type", "application/octet-stream");
+            unityWebRequest.SetRequestHeader(ocpApimSubscriptionKeyHeader, authorizationKey);
+
+            // the download handler will help receiving the analysis from Azure
+            unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+
+            // the upload handler will help uploading the byte array with the request
+            unityWebRequest.uploadHandler = new UploadHandlerRaw(imageBytes);
+            unityWebRequest.uploadHandler.contentType = "application/octet-stream";
+
+            yield return unityWebRequest.SendWebRequest();
+
+            long responseCode = unityWebRequest.responseCode;
+
+            try
+            {
+                string jsonResponse = null;
+                jsonResponse = unityWebRequest.downloadHandler.text;
+
+                // The response will be in Json format
+                // therefore it needs to be deserialized into the classes AnalysedObject and TagData
+                AnalysedObject analysedObject = new AnalysedObject();
+                analysedObject = JsonUtility.FromJson<AnalysedObject>(jsonResponse);
+
+                if (analysedObject.tags == null)
+                {
+                    Debug.Log("analysedObject.tagData is null");
+                }
+                else
+                {
+                    Dictionary<string, string> tagsDictionary = new Dictionary<string, string>();
+
+                    foreach (TagData td in analysedObject.tags)
+                    {
+                        TagData tag = td as TagData;
+                        if ("fruit".Equals(tag.name, StringComparison.OrdinalIgnoreCase))
+                        {
+                            tagsDictionary = new Dictionary<string, string>();
+                            tagsDictionary.Add(tag.name, "calories:52kcal\nfat:0.2g\npolyunsatured fat:0.1g\ncholesterol:0mg\nsodium:1mg\npotassium:107mg\ncarbs:14g\nfood fiber:1.4g\nsugar: 10g\nproteins:0.3g");
+                            break;
+                        }
+                        else
+                        {
+                            tagsDictionary.Add(tag.name, tag.confidence.ToString("0.00 \n"));
+                        }
+                    }
+
+                    ResultsLabel.instance.SetTagsToLastLabel(tagsDictionary);
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.Log("Json exception.Message: " + exception.Message);
+            }
+
+            yield return null;
+        }
+    }
+}
